f66df1aeab60250d33a0fe79b4e00f8ce4181359
ghitasebbar <54407830+ghitasebbar@users.noreply.github.com>
2022-07-06 16:56:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
-docker container stop projet2-api
 
-docker container rm projet2-api
-
-docker image rm orange_de/projet2-api:latest
+minikube addons enable ingress
 
 docker image build . -t orange_de/projet2-api:latest
 
-docker run -d -p 8000:8000 --name projet2-api orange_de/projet2-api:latest
