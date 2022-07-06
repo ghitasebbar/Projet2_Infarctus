b848aae653ba92f9724474526361686ea3ae6c22
ghitasebbar <54407830+ghitasebbar@users.noreply.github.com>
2022-07-06 16:59:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 
+eval $(minikube -p minikube docker-env)
+
 minikube addons enable ingress
+
 
 docker image build . -t orange_de/projet2-api:latest
 
+kubectl create -f my-api-deployement.yml
+
+kubectl create -f my-api-exposition.yml
+
+kubectl create -f my-api-exposition-ingress.yml
